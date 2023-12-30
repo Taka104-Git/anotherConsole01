64e6505202ebd4c6f846db85aa26a7cab89a11f2
西岡隆 <@DESKTOP-0RMBR3H>
2023-12-30 10:59:27 +0900

    プロジェクト ファイルを追加します。

---
+++ anotherConsole01.cpp
@@ -0,0 +1,49 @@
+﻿// main.cpp
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
+{
+    MSG  msg;
+    WNDCLASSW wc = { 0 };
+
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpszClassName = L"Window";
+    wc.hInstance = hInstance;
+    wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+    wc.lpfnWndProc = WndProc;
+    wc.hCursor = LoadCursor(0, IDC_ARROW);
+
+    RegisterClassW(&wc);
+    CreateWindowW(wc.lpszClassName, L"Window",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        100, 100, 250, 150, NULL, NULL, hInstance, NULL);
+
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg) {
+      case WM_PAINT: {
+        PAINTSTRUCT ps;
+
+        HDC hdc = BeginPaint(hwnd, &ps);
+        TextOutW(hdc, 50, 50, L"TEST", 4);
+        EndPaint(hwnd, &ps);
+        break;
+      }
+
+      case WM_DESTROY: {
+        PostQuitMessage(0);
+        return 0;
+      }
+    }
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
